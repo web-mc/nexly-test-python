809c091cdd0d4f078dafb6b865d68d6378f62541
web-mc <peshkov.mc@gmail.com>
2024-10-10 18:48:58 +0300

    удалил лишние импорты

--- validators/abstract_validator.py
+++ validators/abstract_validator.py
@@ -1,15 +1,8 @@
 from abc import ABC
 from typing import Any
 
-from exctractors import PdfExctarctor
-
 
 class BaseValidator(ABC):
-    """
-    name: Name of validator.
-    page_number: The page from which need to get data for validation.
-    """
-
     name: str
 
     def validate(self, value: Any) -> None:
